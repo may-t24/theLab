b57c6505fef276ad87b110aa2225e6bfeef97c7f
Desmond Hui <valueportal@gmail.com>
2025-11-27 11:41:38 -0700

    made player.* doxygen compatible

--- player.c
+++ player.c
@@ -1,21 +1,28 @@
-//Group 2 - Assignment 2
+/********************************************************
+ * @file player.c                                       *
+ * @Brief Definition of the functions in player.h       *
+ * @author CMPT201 Group 2                              *
+ * @date Nov. 27, 2025                                  *
+ ********************************************************/
 #include <stdio.h>
 #include <curses.h>
 #include <unistd.h>
 #include "player.h"
 
-Player createPlayer(){
+Player createPlayer(int x, int y){
 	Player p1;
-	p1.x = 10;
-	p1.y = 10;
+	p1.x = x;
+	p1.y = y;
 	p1.symbol = '@';
 	return p1;
 }
 
+/* might need this for other functions
 void drawPlayer(){
 	mvaddch(p1.y,p1.s,p1.symbol);
 	refresh();
 }
+*/
 
 Player moveRight(Player p1){
 	mvaddch(p1.y,p1.x,' ');
--- player.h
+++ player.h
@@ -1,21 +1,58 @@
-//Group 2 -Assignment 2
+/********************************************************
+ * @file player.h                                       *
+ * @Public Interface of the player                      *
+ * @author CMPT201 Group 2                              *
+ * @date Nov. 27, 2025                                  *
+ ********************************************************/
+
+
 #ifndef PLAYER_H
 #define PLAYER_H
-//STRUCT START
 
+/**
+* @brief This Struct defines the player
+*/
 typedef struct{
-        int x;
-	int y;
-	char symbol;
+        int x; /**< initial x of the player */
+	int y; /**< initial y of the player */
+	char symbol; /**< character symbol representing the character*/
 }Player;
 
-// STRUCT END
-Player createPlayer(void);
+/**
+* This function creates a new player at the provided coordinate
+* @param x is an integer representing the initial x of the player
+* @param y is an integer representing the initial y of the player
+* @return the created player as type Player
+*/
+Player createPlayer(int x, int y);
 
-void drawPlayer(Player p1);
+//void drawPlayer(Player p1);
 
+/**
+* This function moves the player to the right by 1
+* @param p1 is the Player
+* @return Player with an updated position
+*/
 Player moveRight(Player p1);
+
+/**
+* This function moves the player to the left by 1 
+* @param p1 is the Player
+* @return Player with an updated position
+*/
 Player moveLeft(Player p1);
+
+/**
+* This function moves the player to the up by 1 
+* @param p1 is the Player
+* @return Player with an updated position
+*/
 Player moveUp(Player p1);
+
+/**
+* This function moves the player to the down by 1 
+* @param p1 is the Player
+* @return Player with an updated position
+*/
 Player moveDown(Player p1);
 #endif
