3a1f5651c565496fa7118bd36bf0e66e13d8741b
Desmond Hui <valueportal@gmail.com>
2025-12-02 16:03:10 -0700

    implemented key check and npc timer in level_template

--- level_template.c
+++ level_template.c
@@ -1,7 +1,6 @@
 //Group 2 - Assignment 2
 #include <stdio.h>
 #include <curses.h>
-#include <unistd.h>
 #include <time.h>
 /*This is just a template of what a level*.c could look like.   Needs to be expanded more
 This is where you would add the maze layout and the objects.  Basically think about how the level would look like and translate it into here
@@ -10,10 +9,13 @@ void level_template(){
         char h = '|';
 	//represents the object the player needs to collect.  Replace 0 with the value for the level
         int key_count =0;
+	//NPC not a struct since its fairly similar to Player struct without much differences
 	Player NPC;
 	NPC.createPlayer(10,15);
 	//The npcs dialogue when you interact with him.  Replace example text with your own text
 	char npc_dialogue[] ="Hello Player";
+	time_t npc_timer1 = time();
+	time_t npc_timer2 = time();
         //Adds actual walls since border is visual only
         mvprintw(25,30,"%c",h);
         mvprintw(26,30,"%c",h);
@@ -22,11 +24,20 @@ void level_template(){
         drawPlayer(p1);
         //Handles input / gamestate
         while(1){
-		//Conceptual code where if key_count =0, then = turns into blank
-		//Simple NPC cycle.  Dont place him in a wall
-		//conceptual code, will work later
-		/* if time is 3 seconds, NPC move right, 
-		if time is 6 seconds NPC move left reset time		*/
+		//Keep track of where = is in the maze.  Replace 0,0 with the coords of =
+		if key_count = 0{
+			mvaddch("0","0");
+		}
+		/* if timer1 is 3 seconds, NPC move right, 
+		if timer2 is 6 seconds NPC move left reset time		*/
+		if(time - npc_timer1 >=3){
+			movePlayerRight(NPC);
+			npc_timer1 = time();
+		}
+		if (time - npc_timer2 >=6){
+			movePlayerLeft(NPC);
+			npc_timer2 = time();
+		}
 		//Might see if switch case is possible here
                 if (input == KEY_RIGHT){
                         if (mvinch(p1.y,p1.x+1) == ' '){
